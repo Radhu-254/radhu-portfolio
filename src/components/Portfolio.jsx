8a79810bdcc7a2c68de2eb3dec7210d787d6e953
Radhu-254 <radhu.kalathiya1@gmail.com>
2023-03-23 19:51:10 +0530

    changes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -17,32 +17,32 @@ const Portfolio = () => {
             id:1,
             image:p1,
             title:"Weather App",
-            github:"https://google.com",
-            demo:"https://nodejs.org/en/"
+            github:"hhttps://github.com/Radhu-254/weather-tailwind-react.git",
+            demo:"https://weather-tailwind-react-radhukalathiya1-gmailcom.vercel.app/"
 
         },
         {
             id:2,
             image:p2,
             title:"Odan Infotech Website",
-            github:"https://google.com",
-            demo:"https://nodejs.org/en/"
+            github:"https://github.com/Radhu-254/odan-dark-mode-app.git",
+            demo:"https://odan-dark-mode-app-radhukalathiya1-gmailcom.vercel.app/"
 
         },
         {
             id:3,
             image:p3,
             title:"Harry Potter Website",
-            github:"https://google.com",
-            demo:"https://nodejs.org/en/"
+            github:"https://github.com/Radhu-254/harry-potter.git",
+            demo:"https://harry-potter-xyz-radhukalathiya1-gmailcom.vercel.app/"
 
         },
         {
             id:4,
             image:p4,
             title:"Japanese Restaurant Website ",
-            github:"https://google.com",
-            demo:"https://nodejs.org/en/"
+            github:"https://github.com/Radhu-254/adachi-restaurant.git",
+            demo:"https://adachi-restaurant-radhukalathiya1-gmailcom.vercel.app/"
 
         },
         {
@@ -68,44 +68,44 @@ const Portfolio = () => {
     <Section
         title="Portfolio 📜"
         subtitle= "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.">
-     
+
      <div className="grid gap-8 lg:gap-14 lg:grid-cols-2">
 
        {projects.map(({id,image,title,github,demo})=>(
 
             <div
-                key={id} 
+                key={id}
                 className="max-w-lg flex shadow-lg shadow-gray-300 rounded-2xl overflow-hidden"
             >
                 <img src={image} alt={title} className="w-2/3"/>
 
-                <div 
+                <div
                     className='w-1/3 flex flex-col items-center justify-evenly p-1'
                     >
                     <h2>{title}</h2>
-                    <a 
-                        href={github} 
-                        target="_blank" 
+                    <a
+                        href={github}
+                        target="_blank"
                         rel="noopener noreferrer"
                         className="text-2xl cursor-pointer duration-150 hover:scale-110"
-                        > 
+                        >
                         <UilGithub/>
                     </a>
-                    <a href={demo}  
-                        target="_blank" 
+                    <a href={demo}
+                        target="_blank"
                         rel="noopener noreferrer"
                         className="text-2xl cursor-pointer duration-150 hover:scale-110"
-                        > 
+                        >
                         <UilExternalLinkAlt/>
                     </a>
                 </div>
-                
+
 
             </div>
 
        ))}
 
-   
+
      </div>
     </Section>
   )
