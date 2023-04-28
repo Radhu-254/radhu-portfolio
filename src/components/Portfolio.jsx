c44e0617f10724996a0e6d589c80fd10166539fe
Radhu-254 <radhu.kalathiya1@gmail.com>
2023-04-28 10:15:59 +0530

    added project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,6 +10,7 @@ import p4 from "../assets/p4.png";
 import p5 from "../assets/p5.png";
 import p6 from "../assets/p6.png";
 import p7 from "../assets/p7.png";
+import p8 from "../assets/p8.png"
 
 
 
@@ -20,55 +21,63 @@ const Portfolio = () => {
             id:1,
             image:p1,
             title:"Weather App",
-            github:"https://google.com",
-            demo:"https://weather-tailwind-react-radhukalathiya1-gmailcom.vercel.app/"
+            github:"https://github.com/Radhu-254/weather-tailwind-react.git",
+            demo:"https://weather-tailwind-react-radhukalathiya1-gmailcom.vercel.app"
 
         },
         {
             id:2,
             image:p2,
             title:"Odan Infotech Website",
-            github:"https://google.com",
-            demo:"https://odan-dark-mode-app-radhukalathiya1-gmailcom.vercel.app/"
+            github:"https://github.com/Radhu-254/odan-dark-mode-app.git",
+            demo:"https://odan-dark-mode-app-radhukalathiya1-gmailcom.vercel.app"
 
         },
         {
             id:3,
             image:p3,
-            title:"Harry Potter Website",
-            github:"https://google.com",
-            demo:"https://harry-potter-xyz-radhukalathiya1-gmailcom.vercel.app/"
+            title:"Harry Potter Childern's Website",
+            github:"https://github.com/Radhu-254/harry-potter.git",
+            demo:"https://harry-potter-xyz-radhukalathiya1-gmailcom.vercel.app"
 
         },
         {
             id:4,
             image:p4,
             title:"Japanese Restaurant Website ",
-            github:"https://google.com",
-            demo:"https://adachi-restaurant-radhukalathiya1-gmailcom.vercel.app/"
+            github:"https://github.com/Radhu-254/adachi-restaurant.git",
+            demo:"https://adachi-restaurant-radhukalathiya1-gmailcom.vercel.app"
 
         },
         {
             id:5,
             image:p5,
             title:"An Artist's Portfolio Website",
-            github:"https://google.com",
+            github:"https://github.com/Radhu-254/artist-portfolio.git",
             demo:"https://artist-portfolio-radhukalathiya1-gmailcom.vercel.app"
 
         },
            {
             id:6,
             image:p6,
-            title:"Broc N Roll ",
-            github:"https://google.com",
+            title:"Broc N Roll Recipe Website",
+            github:"https://github.com/Radhu-254/food_blog.git",
             demo:"https://food-blog-git-main-radhukalathiya1-gmailcom.vercel.app"
         },
         {
             id:7,
             image:p7,
             title:"DogGo Grooming Website",
-            github:"https://google.com",
+            github:"https://github.com/Radhu-254/doggo-grooming.git",
             demo:"https://doggo-grooming-radhukalathiya1-gmailcom.vercel.app"
+
+        },
+        {
+            id:8,
+            image:p8,
+            title:"RenCar Car Rental Website",
+            github:"https://github.com/Radhu-254/car-rent.git",
+            demo:"https://car-rent-radhukalathiya1-gmailcom.vercel.app"
 
         }
 
