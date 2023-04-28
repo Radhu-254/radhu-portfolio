bfd8ffcd1276c2fd479e61b17a9a8a96825f485e
Radhu-254 <radhu.kalathiya1@gmail.com>
2023-04-28 10:24:01 +0530

    added service

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Section from './common/Section'
 
 import react from "../assets/service1.svg"
-import node from "../assets/service2.webp"
+import framer from "../assets/service2.webp"
 import mongodb from "../assets/service3.png"
 import tailwind from "../assets/service4.png"
 
@@ -18,9 +18,9 @@ const Services = () => {
         },
         {
             id: 2,
-            image:node,
-            title:"Node JS",
-            link:"https://nodejs.org/"
+            image:tailwind,
+            title:"Tailwind CSS",
+            link:"https://tailwindcss.com/"
         },
         {
             id: 3,
@@ -30,14 +30,14 @@ const Services = () => {
         },
         {
             id: 4,
-            image:tailwind,
-            title:"Tailwind CSS",
-            link:"https://tailwindcss.com/"
+            image:framer,
+            title:"Framer motion",
+            link:"https://www.framer.com/motion/"
         },
     ];
 
   return (
-    <Section 
+    <Section
         title="Skills 🛠️"
         subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
     >
@@ -45,19 +45,19 @@ const Services = () => {
         <div className="grid gap-10 lg:grid-cols-2">
         {
             services.map(({id,image,title,link}) =>(
-                <div 
+                <div
                     key={id}
-                    className="flex flex-col items-center justify-center p-5 shadow-lg dark:shadow-gray-100 rounded-xl 
+                    className="flex flex-col items-center justify-center p-5 shadow-lg dark:shadow-gray-100 rounded-xl
                     duration-300 ease-in-out hover:scale-110">
 
-                    <a 
+                    <a
                       href={link}
                       target="_blank"
                        rel="noopener noreferrer">
-                            <img 
+                            <img
                                 src={image}
-                                alt={title} 
-                                className="w-36 h-36 md:w-44 md:h-44"  
+                                alt={title}
+                                className="w-36 h-36 md:w-44 md:h-44"
                             />
                     </a>
 
